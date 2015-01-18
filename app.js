06f2a3f2b14583ce256d7be654832eb22238d539
Tejas Manohar <me@tejasmanohar.com>
2015-01-17 21:06:07 -0500

    refactor code

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.use(express.static(__dirname + '/tmp'));
 var bodyParser = require('body-parser');
 app.use(bodyParser());
 
-var async = require('async');
 var fs = require('fs');
 var request = require('superagent');
 var twilio = require('twilio');
@@ -31,10 +30,25 @@ app.get('/', function(req, res) {
   res.sendStatus(200);
 });
 
-app.post('/phone-receive', function(req, res) {
-  search(req.body.Body, function(url) {
-    startCall(url);
-  });
+app.post('/receive', function(req, res) {
+  function search(query, cb) {
+    request
+      .get('http://partysyncwith.me:3005/search/'+ query +'/1')
+      .end(function(err, res) {
+        if(err) {
+          console.log(err);
+        } else {
+          if (typeof JSON.parse(res.text).data !== 'undefined') {
+            if (JSON.parse(res.text).data[0].duration < 600) {
+              var url = JSON.parse(res.text).data[0].video_url;
+              cb(url);
+            } else {
+              cb(null);
+          }
+        }
+      }
+    })
+  }
 
   function startCall(url) {
     if (exec('youtube-dl --extract-audio --prefer-ffmpeg --audio-format mp3 --audio-quality 9 -o "tmp/%(id)s.%(ext)s" ' + url).code === 0) {
@@ -46,62 +60,14 @@ app.post('/phone-receive', function(req, res) {
     }
   }
 
-  res.sendStatus(200);
-});
-
-app.post('/app-receive', function(req, res) {
   search(req.body.Body, function(url) {
-    startTransfer(url);
+    startCall(url);
   });
 
-  function startTransfer(url) {
-    if (exec('youtube-dl --extract-audio --prefer-ffmpeg --audio-format mp3 --audio-quality 9 -o "tmp/%(id)s.%(ext)s" ' + url).code === 0) {
-      fs.readFile('tmp/' + url.substring(url.length - 11) + '.mp3', function(err, data) {
-        var str = data.toString('base64');
-        for(var i = 0; i < str.length; i += 3000) {
-          client.sendMessage({
-              to: req.body.From,
-              body: '',
-              mediaUrl: 'http://api.qrserver.com/v1/create-qr-code/?data=' + str.substring(i, Math.min(str.length, i + 3000)),
-              from: process.env.APP_NUMBER,
-            }, function(err, messageData) {
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log(messageData.status);
-                }
-            });
-            if (exec('sleep 1000').code === 0) {
-              continue;
-            }
-        }
-      });
-    }
-  }
-
-  res.send('complete');
+  res.sendStatus(200);
 });
 
 app.post('/xml/:id', function(req, res) {
   res.set('Content-Type', 'text/xml');
   res.send('<Response><Play>' + baseUrl + '/' + req.params.id + '.mp3' + '</Play><Redirect/></Response>');
 });
-
-function search(query, cb) {
-  request
-    .get('http://partysyncwith.me:3005/search/'+ query +'/1')
-    .end(function(err, res) {
-      if(err) {
-        console.log(err);
-      } else {
-        if (typeof JSON.parse(res.text).data !== 'undefined') {
-            if (JSON.parse(res.text).data[0].duration < 600) {
-              var url = JSON.parse(res.text).data[0].video_url;
-              cb(url);
-           } else {
-              cb(null);
-           }
-        }        
-      }
-    })
-}
