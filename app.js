03ca1d1f0a6c298f47764759bfb836f07490f482
Tejas Manohar <me@tejas.io>
2015-03-06 19:14:53 -0600

    crappy code that works. needs callbacks

--- app.js
+++ app.js
@@ -22,9 +22,8 @@ server.listen(port);
 console.log('Listening at port: ' + port);
 
 app.post('/incoming', function(req, res) {
-  searchMusic(req.body.Body, function(url) {
-    startCall(url, req.body.From);
-  });
+  startCall(req.body.Body, req.body.From);
+  res.sendStatus(200);
 });
 
 app.post('/xml/:id', function(req, res) {
@@ -32,31 +31,12 @@ app.post('/xml/:id', function(req, res) {
   res.send('<Response><Play>' + process.env.BASE_URL + '/' + req.params.id + '.mp3' + '</Play><Redirect/></Response>');
 });
 
-function startCall(url, recipient) {
-  if (exec('youtube-dl --extract-audio --prefer-ffmpeg --audio-format mp3 --audio-quality 0 -o "tmp/%(id)s.%(ext)s" ' + url).code === 0) {
-    var call = client.calls.create({
-      to: recipient,
-      from: process.env.PHONE_NUMBER,
-      url: process.env.BASE_URL + '/xml/' + url.substring(url.length - 11)
-    });
-  }
-}
-
-function searchMusic(query, cb) {
-  request
-    .get('http://partysyncwith.me:3005/search/'+ query +'/1')
-    .end(function(err, res) {
-      if(err) {
-        console.log(err);
-      } else {
-        if (typeof JSON.parse(res.text).data !== 'undefined') {
-          if (JSON.parse(res.text).data[0].duration < 600) {
-            var url = JSON.parse(res.text).data[0].video_url;
-            cb(url);
-          } else {
-            cb(null);
-          }
-        }
-      }
-    })
+function startCall(search, recipient) {
+  exec("youtube-dl --extract-audio --prefer-ffmpeg --audio-format mp3 --audio-quality 0 -o \"tmp/" + search + ".%(ext)s\" \"ytsearch:" + search + "\"");
+  exec('sleep 30');
+  var call = client.calls.create({
+    to: recipient,
+    from: process.env.PHONE_NUMBER,
+    url: process.env.BASE_URL + '/xml/' + search
+  });
 }
